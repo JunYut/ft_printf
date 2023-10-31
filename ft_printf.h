4743295d58d33bc155837a9f5d6538e522a1ee3f
Tan Jun Yu <tjun-yu@u82z01s05.42kl.edu.my>
2023-10-31 10:15:31 +0800

    ft_printf.h: - header guard completed - included necessary header files (might add more along the development) - added function prototype of ft_printf()

---
+++ ft_printf.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tjun-yu <tanjunyu8888@gmail.com>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/31 08:41:12 by tjun-yu           #+#    #+#             */
+/*   Updated: 2023/10/31 10:01:07 by tjun-yu          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+# include <stdarg.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+
+# include "libft/libft.h"
+
+int	ft_printf(const char*, ...);
+
+#endif
