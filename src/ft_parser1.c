14dc26150b735dec72877d549ca90a8a30ae0d0b
Tan Jun Yu <tjun-yu@u82z01s05.42kl.edu.my>
2023-11-03 09:52:41 +0800

    ft_parser1: created to handle more than 5 parser functions ft_parser & ft_parser1: prototype for parser functions created

--- src/ft_parser1.c
+++ src/ft_parser1.c
@@ -1,38 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_parser.c                                        :+:      :+:    :+:   */
+/*   ft_parser1.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tjun-yu <tanjunyu8888@gmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/02 15:52:38 by tjun-yu           #+#    #+#             */
-/*   Updated: 2023/11/03 09:51:45 by tjun-yu          ###   ########.fr       */
+/*   Created: 2023/11/03 09:49:02 by tjun-yu           #+#    #+#             */
+/*   Updated: 2023/11/03 09:51:24 by tjun-yu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../ft_printf.h"
 
-static char	*char_parser(va_list args)
+static char	* unsigned_parser(va_list args)
 {
 	
 }
 
-static char	*str_parser(va_list args)
+static char	* low_hexa_parser(va_list args)
 {
 	
 }
 
-static char	*ptr_parser(va_list args)
-{
-	
-}
-
-static char	*decimal_parser(va_list args)
-{
-	
-}
-
-static char	*int_parser(va_list args)
+static char	* up_hexa_parser(va_list args)
 {
 	
 }
