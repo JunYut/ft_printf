f24ee785b4c9108391e1b5a9090472635a5a3ae8
Tan Jun Yu <tjun-yu@u82z01s05.42kl.edu.my>
2023-11-07 10:04:49 +0800

    str_parser: now dynamically allocates memory for the arg string to be freed at main function

--- src/str_parser.c
+++ src/str_parser.c
@@ -6,7 +6,7 @@
 /*   By: tjun-yu <tanjunyu8888@gmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/03 15:33:38 by tjun-yu           #+#    #+#             */
-/*   Updated: 2023/11/03 15:33:50 by tjun-yu          ###   ########.fr       */
+/*   Updated: 2023/11/07 10:03:11 by tjun-yu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,15 @@
 const char	*str_parser(va_list args)
 {
 	char	*parsed_arg;
+	char	*arg;
+	size_t	len;
 
-	parsed_arg = va_arg(args, char *);
+	arg = va_arg(args, char *);
+	len = ft_strlen(arg);
+	parsed_arg = (char *)malloc((len + 1) * sizeof(char));
+	if (parsed_arg == NULL)
+		return (NULL);
+	ft_strlcpy(parsed_arg, arg, len + 1);
+	parsed_arg[len] = 0;
 	return (parsed_arg);
 }
