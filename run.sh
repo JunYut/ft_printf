067a5eecb33a557ca710fd6919449837ff4cd4dd
Tan Jun Yu <tjun-yu@u82z01s05.42kl.edu.my>
2023-11-07 09:10:38 +0800

    run: - now compile and executes main.c using libftprint.a - enabled options to run script with or without leak check

--- run.sh
+++ run.sh
@@ -1,2 +1,9 @@
-cc -Wall -Wextra -Werror src/*.c -o a.out -Llibft -lft
-./a.out
+LEAK_CHECK=0
+
+cc -Wall -Wextra -Werror *.c -o a.out -Llibft -L. -lft -lftprintf
+
+if [[ LEAK_CHECK -eq 1 ]]; then
+	leaks -atExit -- ./a.out
+else
+	./a.out
+fi
