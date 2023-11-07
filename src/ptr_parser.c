840702e30b958580522c9da12a27ee1fef4a4843
Tan Jun Yu <tjun-yu@u82z01s05.42kl.edu.my>
2023-11-07 13:12:33 +0800

    ptr_parser: defined modified low_hexa_parser due to formatting reasons

--- src/ptr_parser.c
+++ src/ptr_parser.c
@@ -6,13 +6,29 @@
 /*   By: tjun-yu <tanjunyu8888@gmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/03 15:32:00 by tjun-yu           #+#    #+#             */
-/*   Updated: 2023/11/06 15:23:19 by tjun-yu          ###   ########.fr       */
+/*   Updated: 2023/11/07 13:11:16 by tjun-yu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../ft_printf.h"
 
+static const char	*hexa_parser(va_list args)
+{
+	char		*parsed_arg;
+	uintptr_t	arg;
+	char		*low_hexa;
+
+	parsed_arg = (char *)malloc(64 * sizeof(char));
+	if (parsed_arg == NULL)
+		return (NULL);
+	ft_strlcpy(parsed_arg, "0x", 64);
+	arg = va_arg(args, uintptr_t);
+	low_hexa = decimal_to_hexa(arg);
+	ft_strlcat(parsed_arg, low_hexa, 64);
+	return (parsed_arg);
+}
+
 const char	*ptr_parser(va_list args)
 {
-	return(low_hexa_parser(args));
+	return(hexa_parser(args));
 }
