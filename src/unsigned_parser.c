6431afcb1db396b1d0f21369dd76bdf0b94d7371
Tan Jun Yu <tjun-yu@u82z01s05.42kl.edu.my>
2023-11-07 13:09:22 +0800

    unsigned_parser: now handles underflow

--- src/unsigned_parser.c
+++ src/unsigned_parser.c
@@ -6,7 +6,7 @@
 /*   By: tjun-yu <tanjunyu8888@gmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/03 15:35:22 by tjun-yu           #+#    #+#             */
-/*   Updated: 2023/11/06 15:57:46 by tjun-yu          ###   ########.fr       */
+/*   Updated: 2023/11/07 12:59:48 by tjun-yu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,10 @@
 
 const char	*unsigned_parser(va_list args)
 {
-	return (decimal_parser(args));
+	char			*parsed_arg;
+	unsigned int	arg;
+	
+	arg = va_arg(args, unsigned int);
+	parsed_arg = ft_itoa(arg);
+	return (parsed_arg);
 }
