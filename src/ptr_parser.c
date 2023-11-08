05f400110f4b04f859673fd344014f92cb8cdb29
Tan Jun Yu <tjun-yu@u82z01s05.42kl.edu.my>
2023-11-08 13:50:50 +0800

    ptr_parser: now handles 0 as an argument

--- src/ptr_parser.c
+++ src/ptr_parser.c
@@ -6,7 +6,7 @@
 /*   By: tjun-yu <tanjunyu8888@gmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/03 15:32:00 by tjun-yu           #+#    #+#             */
-/*   Updated: 2023/11/08 13:17:27 by tjun-yu          ###   ########.fr       */
+/*   Updated: 2023/11/08 13:48:37 by tjun-yu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,13 @@ static const char	*hexa_parser(va_list args)
 	unsigned long long	arg;
 	char		*low_hexa;
 
+	arg = va_arg(args, unsigned long long);
+	if (arg == 0)
+		return (ft_strdup("0x0"));
 	parsed_arg = (char *)malloc(64 * sizeof(char));
 	if (parsed_arg == NULL)
 		return (NULL);
 	ft_strlcpy(parsed_arg, "0x", 64);
-	arg = va_arg(args, unsigned long long);
 	low_hexa = decimal_to_hexa(arg);
 	ft_strlcat(parsed_arg, low_hexa, 64);
 	return (parsed_arg);
