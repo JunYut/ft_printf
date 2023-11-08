a26ccdc4008f69932de79de395a3a5ee1ffd5075
Tan Jun Yu <tjun-yu@u82z01s05.42kl.edu.my>
2023-11-08 15:32:09 +0800

    str_parser: (void)

--- src/str_parser.c
+++ src/str_parser.c
@@ -6,7 +6,7 @@
 /*   By: tjun-yu <tanjunyu8888@gmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/03 15:33:38 by tjun-yu           #+#    #+#             */
-/*   Updated: 2023/11/08 13:12:51 by tjun-yu          ###   ########.fr       */
+/*   Updated: 2023/11/08 15:01:40 by tjun-yu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,7 @@ const char	*str_parser(va_list args)
 	parsed_arg = (char *)malloc((len + 1) * sizeof(char));
 	if (parsed_arg == NULL)
 		return (NULL);
+	*parsed_arg = 0;
 	ft_strlcpy(parsed_arg, arg, len + 1);
 	parsed_arg[len] = 0;
 	return (parsed_arg);
