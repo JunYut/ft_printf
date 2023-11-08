291f4767f2cb116c7bc6ffe6dfabf3827186aa83
Tan Jun Yu <tjun-yu@u82z01s05.42kl.edu.my>
2023-11-08 15:33:18 +0800

    ptr_parser: (void)

--- src/ptr_parser.c
+++ src/ptr_parser.c
@@ -6,25 +6,16 @@
 /*   By: tjun-yu <tanjunyu8888@gmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/03 15:32:00 by tjun-yu           #+#    #+#             */
-/*   Updated: 2023/11/08 14:41:50 by tjun-yu          ###   ########.fr       */
+/*   Updated: 2023/11/08 15:28:01 by tjun-yu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../ft_printf.h"
 
-static const char	*hexa_parser(va_list args)
+static const char	*hexa_parser(unsigned long long arg, char * parsed_arg)
 {
-	char		*parsed_arg;
-	unsigned long long	arg;
-	char		*low_hexa;
-
-	arg = va_arg(args, unsigned long long);
-	if (arg == 0)
-		return (ft_strdup("0x0"));
-	parsed_arg = (char *)malloc(64 * sizeof(char));
-	if (parsed_arg == NULL)
-		return (NULL);
-	ft_strlcpy(parsed_arg, "0x", 64);
+	char	*low_hexa;
+	
 	low_hexa = decimal_to_hexa(arg);
 	ft_strlcat(parsed_arg, low_hexa, 64);
 	return (parsed_arg);
@@ -32,5 +23,16 @@ static const char	*hexa_parser(va_list args)
 
 const char	*ptr_parser(va_list args)
 {
-	return(hexa_parser(args));
+	char				*parsed_arg;
+	unsigned long long	arg;
+	
+	arg = va_arg(args, unsigned long long);
+	if (arg == 0)
+		return (ft_strdup("0x0"));
+	parsed_arg = (char *)malloc(64 * sizeof(char));
+	if (parsed_arg == NULL)
+		return (NULL);
+	*parsed_arg = 0;
+	ft_strlcpy(parsed_arg, "0x", 64);
+	return(hexa_parser(arg, parsed_arg));
 }
