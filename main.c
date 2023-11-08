a04d7f955ff4aee18d9d7661bcd2794888124f20
Tan Jun Yu <tjun-yu@u82z01s05.42kl.edu.my>
2023-11-08 10:56:09 +0800

    main.c: now will display return count

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include "ft_printf.h"
 
 #define DIVIDER printf("---------------------------------------------------\n")
+#define LOG_RETURN(RETURN) printf("%d\n", RETURN)
 
 //_____________________________________________________________________________
 void char_tester();
@@ -11,7 +12,6 @@ void int_tester();
 void unsigned_tester();
 void low_hexa_tester();
 void up_hexa_tester();
-void empty_NULL_tester();
 
 int main(void)
 {
@@ -35,62 +35,73 @@ int main(void)
 
 void char_tester()
 {
-	printf("Original: %c\n", 'C');
-	ft_printf("ft_:      %c\n", 'C');
+	printf("char_tester:\n\n");
+
+	LOG_RETURN(printf("Original: %c\n", 'C'));
+	LOG_RETURN(ft_printf("ft_:      %c\n", 'C'));
 	DIVIDER;
 }
 
 void str_tester()
 {
-	printf("Original: %s\n", "Hello World!");
-	ft_printf("ft_:      %s\n", "Hello World!");
+	printf("str_tester:\n\n");
+
+	LOG_RETURN(printf("Original: %s\n", "Hello World!"));
+	LOG_RETURN(ft_printf("ft_:      %s\n", "Hello World!"));
 	DIVIDER;
 }
 
 void ptr_tester(int *ptr)
 {
-	printf("Original: %p\n", ptr);
-	ft_printf("ft_:      %p\n", ptr);
+	printf("ptr_tester:\n\n");
+
+	LOG_RETURN(printf("Original: %p\n", ptr));
+	LOG_RETURN(ft_printf("ft_:      %p\n", ptr));
 	DIVIDER;
 }
 
 void decimal_tester()
 {
-	printf("Original: %d\n", 69420);
-	ft_printf("ft_:      %d\n", 69420);
+	printf("decimal_tester:\n\n");
+
+	LOG_RETURN(printf("Original: %d\n", 69420));
+	LOG_RETURN(ft_printf("ft_:      %d\n", 69420));
 	DIVIDER;
 }
 
 void int_tester()
 {
-	printf("Original: %i\n", 69420);
-	ft_printf("ft_:      %i\n", 69420);
+	printf("int_tester:\n\n");
+
+	LOG_RETURN(printf("Original: %i\n", 69420));
+	LOG_RETURN(ft_printf("ft_:      %i\n", 69420));
 	DIVIDER;
 }
 
 void unsigned_tester()
 {
-	printf("Original: %u\n", -69420);
-	ft_printf("ft_:      %u\n", -69420);
+	printf("unsigned_tester:\n\n");
+
+	LOG_RETURN(printf("Original: %u\n", -69420));
+	LOG_RETURN(ft_printf("ft_:      %u\n", -69420));
 	DIVIDER;
 }
 
 void low_hexa_tester()
 {
-	printf("Original: %x\n", 1000);
-	ft_printf("ft_:      %x\n", 1000);
+	printf("low_hexa_tester:\n\n");
+
+	LOG_RETURN(printf("Original: %x\n", 1000));
+	LOG_RETURN(ft_printf("ft_:      %x\n", 1000));
 	DIVIDER;
 }
 
 void up_hexa_tester()
 {
-	printf("Original: %X\n", 1000);
-	ft_printf("ft_:      %X\n", 1000);
+	printf("up_hexa_tester:\n\n");
+
+	LOG_RETURN(printf("Original: %X\n", 1000));
+	LOG_RETURN(ft_printf("ft_:      %X\n", 1000));
 	DIVIDER;
 }
 
-void empty_NULL_tester()
-{
-	// printf("Original: %X\n",);
-	ft_printf("ft_:      %X\n");
-}
