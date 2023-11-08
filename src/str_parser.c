838b51bb17b1c9c07bebb76932394117176656ba
Tan Jun Yu <tjun-yu@u82z01s05.42kl.edu.my>
2023-11-08 13:14:56 +0800

    str_parser: now handles NULL argument

--- src/str_parser.c
+++ src/str_parser.c
@@ -6,7 +6,7 @@
 /*   By: tjun-yu <tanjunyu8888@gmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/03 15:33:38 by tjun-yu           #+#    #+#             */
-/*   Updated: 2023/11/07 10:03:11 by tjun-yu          ###   ########.fr       */
+/*   Updated: 2023/11/08 13:12:51 by tjun-yu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ const char	*str_parser(va_list args)
 	size_t	len;
 
 	arg = va_arg(args, char *);
+	if (arg == NULL)
+		return (ft_strdup("(null)"));
 	len = ft_strlen(arg);
 	parsed_arg = (char *)malloc((len + 1) * sizeof(char));
 	if (parsed_arg == NULL)
