260ea8ea4a3e82a19efb0230c3ebc2625c0e7e64
Tan Jun Yu <tjun-yu@u82z01s05.42kl.edu.my>
2023-11-09 11:03:15 +0800

    ptr_parser: - added static function decimal_to_hexa() that handles unsigned long long - passed tripouille

--- src/ptr_parser.c
+++ src/ptr_parser.c
@@ -6,19 +6,27 @@
 /*   By: tjun-yu <tanjunyu8888@gmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/03 15:32:00 by tjun-yu           #+#    #+#             */
-/*   Updated: 2023/11/08 16:05:04 by tjun-yu          ###   ########.fr       */
+/*   Updated: 2023/11/09 10:55:30 by tjun-yu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../ft_printf.h"
 
-static const char	*hexa_parser(unsigned long long arg, char *parsed_arg)
+static void	decimal_to_hexa(unsigned long long decimal, char *parsed_arg)
 {
-	char	*low_hexa;
-	
-	low_hexa = decimal_to_hexa(arg);
-	ft_strlcat(parsed_arg, low_hexa, 64);
-	return (parsed_arg);
+	int		i;
+
+	i = -1;
+	while (decimal != 0)
+	{
+		if (decimal % 16 >= 10)
+			parsed_arg[++i] = decimal % 16 + 'a' - 10;
+		else
+			parsed_arg[++i] = decimal % 16 + '0';
+		decimal /= 16;
+	}
+	parsed_arg[i + 1] = 0;
+	reverse_arr(parsed_arg);
 }
 
 const char	*ptr_parser(va_list args)
@@ -34,5 +42,6 @@ const char	*ptr_parser(va_list args)
 		return (NULL);
 	*parsed_arg = 0;
 	ft_strlcpy(parsed_arg, "0x", 64);
-	return(hexa_parser(args));
+	decimal_to_hexa(arg, parsed_arg + 2);
+	return (parsed_arg);
 }
